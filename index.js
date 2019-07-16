215686e6680d6f29ee8151627902ffce561c53d1
JUNGPILLEE <samuel33227@gmail.com>
2019-07-16 22:01:27 +0900

    before Using bodyParser

--- index.js
+++ index.js
@@ -10,8 +10,29 @@ var users = [
 app.use(morgan('dev'));;
 
 app.get('/users', function(req, res) {
-    res.json(users);
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10);
+    if(Number.isNaN(limit)) {
+        return res.status(400).end();
+    }
+    res.json(users.slice(0, limit));
 });
+
+app.get('/users/:id', function(req, res) {
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.status(400).end();
+
+    const user = users.filter((user) => user.id === id)[0];
+    if(!user) return res.status(404).end();
+    res.json(user);
+})
+
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.status(400).end();
+    users = users.filter(user=> user.id !== id)
+    res.status(204).end();
+})
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
